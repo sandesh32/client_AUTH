4ff8b39e6925aa5fd7234ffcdc4639540219b6be
Sandesh Dhungana <dhunganasandesh32@gmail.com>
2021-07-11 22:29:17 +0545

    Added simple styling

--- src/Components/nav.js
+++ src/Components/nav.js
@@ -4,21 +4,31 @@ import { Link } from 'react-router-dom';
 export default function Header(props) {
     if(localStorage.getItem("token")){  
         return (
-            <div id="navbar">
-            <ul>
-                <li><Link to="/">Home</Link></li>
+            <div className="navbar">
+                <div className="sandesh">
+                    <Link to="/">Sandesh</Link>
+                </div>
+                <div className="nav_links"> 
+                <ul>
+                <li><Link to="/">Home</Link></li> <div className="horline"> | </div>
                 <li><Link to="/logout">Logout</Link></li>
             </ul>
+                </div>
         </div>
     )
 }
 else{
     return (
-        <div id="navbar">
+        <div className="navbar">
+            <div className="sandesh">
+                    <Link to="/">Sandesh</Link>
+                </div>
+            <div className="nav_links"> 
         <ul>
-            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/login">Login</Link></li> <div className="horline"> | </div>  
             <li><Link to="/signup">Signup</Link></li>
         </ul>
+        </div>
     </div>
     )
     }
